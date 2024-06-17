d36d9f569b8d11439cbdef8265dfe5f1f06661f4
ylhong <ylhong@ssrinc.co.kr>
2024-06-17 20:29:34 +0900

    lyricsSearch

--- go.mod
+++ go.mod
@@ -4,4 +4,9 @@ go 1.21.6
 
 require github.com/unidoc/unioffice v1.33.0
 
-require github.com/richardlehane/msoleps v1.0.3 // indirect
+require (
+	github.com/PuerkitoBio/goquery v1.9.2 // indirect
+	github.com/andybalholm/cascadia v1.3.2 // indirect
+	github.com/richardlehane/msoleps v1.0.3 // indirect
+	golang.org/x/net v0.26.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,15 +1,61 @@
-// Copyright 2017 FoxyUtils ehf. All rights reserved.
 package main
 
 import (
 	"fmt"
-	"github.com/unidoc/unioffice/color"
+	"github.com/PuerkitoBio/goquery"
 	"github.com/unidoc/unioffice/common/license"
-	"github.com/unidoc/unioffice/document"
-	"github.com/unidoc/unioffice/measurement"
-	"github.com/unidoc/unioffice/schema/soo/wml"
+	"github.com/unidoc/unioffice/presentation"
+	"log"
+	"net/http"
+	"net/url"
+	"strconv"
 )
 
+// SlideData 구조체는 슬라이드에 포함될 데이터를 나타냅니다.
+type SlideData struct {
+	Title   string
+	Content string
+}
+
+type songInfo struct {
+	trackId int
+}
+
+func (si songInfo) searchLyricsList(recvStr string) {
+	// 검색어 설정
+	searchQuery := url.QueryEscape(recvStr)
+	searchUrl := fmt.Sprintf("https://music.bugs.co.kr/search/lyrics?q=%s", searchQuery)
+	fmt.Println(searchUrl)
+	// HTTP 요청 보내기
+	resp, err := http.Get(searchUrl)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != 200 {
+		log.Fatalf("status code error: %d %s", resp.StatusCode, resp.Status)
+	}
+
+	// HTML 파싱
+	doc, err := goquery.NewDocumentFromReader(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// 각 tr 요소를 탐색하며 albumid와 mark 태그의 텍스트를 추출
+	doc.Find("tr[rowtype='lyrics']").Each(func(i int, s *goquery.Selection) {
+		albumID, exists := s.Attr("trackId")
+		if exists {
+			tempNo, err := strconv.ParseInt(albumID, 10, 64)
+			if err != nil {
+				log.Fatal(err)
+			}
+			si.trackId = int(tempNo)
+			fmt.Printf("AlbumID: %s ", albumID)
+		}
+	})
+}
 func init() {
 	// Make sure to load your metered License API key prior to using the library.
 	// If you need a key, you can sign up and create a free one at https://cloud.unidoc.io
@@ -22,94 +68,50 @@ func init() {
 }
 
 func main() {
-	doc := document.New()
-	defer doc.Close()
-
-	para := doc.AddParagraph()
-	run := para.AddRun()
-	para.SetStyle("Title")
-	run.AddText("Simple Document Formatting")
-
-	para = doc.AddParagraph()
-	para.SetStyle("Heading1")
-	run = para.AddRun()
-	run.AddText("Some Heading Text")
-
-	para = doc.AddParagraph()
-	para.SetStyle("Heading2")
-	run = para.AddRun()
-	run.AddText("Some Heading Text")
-
-	para = doc.AddParagraph()
-	para.SetStyle("Heading3")
-	run = para.AddRun()
-	run.AddText("Some Heading Text")
-
-	para = doc.AddParagraph()
-	para.Properties().SetFirstLineIndent(0.5 * measurement.Inch)
-
-	run = para.AddRun()
-	run.AddText("A run is a string of characters with the same formatting. ")
-
-	run = para.AddRun()
-	run.Properties().SetBold(true)
-	run.Properties().SetFontFamily("Courier")
-	run.Properties().SetSize(15)
-	run.Properties().SetColor(color.Red)
-	run.AddText("Multiple runs with different formatting can exist in the same paragraph. ")
-
-	run = para.AddRun()
-	run.AddText("Adding breaks to a run will insert line breaks after the run. ")
-	run.AddBreak()
-	run.AddBreak()
-
-	run = createParaRun(doc, "Runs support styling options:")
-
-	run = createParaRun(doc, "small caps")
-	run.Properties().SetSmallCaps(true)
-
-	run = createParaRun(doc, "strike")
-	run.Properties().SetStrikeThrough(true)
-
-	run = createParaRun(doc, "double strike")
-	run.Properties().SetDoubleStrikeThrough(true)
-
-	run = createParaRun(doc, "outline")
-	run.Properties().SetOutline(true)
-
-	run = createParaRun(doc, "emboss")
-	run.Properties().SetEmboss(true)
-
-	run = createParaRun(doc, "shadow")
-	run.Properties().SetShadow(true)
-
-	run = createParaRun(doc, "imprint")
-	run.Properties().SetImprint(true)
-
-	run = createParaRun(doc, "highlighting")
-	run.Properties().SetHighlight(wml.ST_HighlightColorYellow)
-
-	run = createParaRun(doc, "underline")
-	run.Properties().SetUnderline(wml.ST_UnderlineWavyDouble, color.Red)
-
-	run = createParaRun(doc, "text effects")
-	run.Properties().SetEffect(wml.ST_TextEffectAntsRed)
-
-	nd := doc.Numbering.Definitions()[0]
-
-	for i := 1; i < 5; i++ {
-		p := doc.AddParagraph()
-		p.SetNumberingLevel(i - 1)
-		p.SetNumberingDefinition(nd)
-		run := p.AddRun()
-		run.AddText(fmt.Sprintf("Level %d", i))
+	// 슬라이드 데이터 생성
+	slides := []SlideData{
+		{"Title 1", "Content 1"},
+		{"Title 2", "Content 2"},
+		{"Title 3", "Content 3"},
 	}
-	doc.SaveToFile("simple.docx")
-}
 
-func createParaRun(doc *document.Document, s string) document.Run {
-	para := doc.AddParagraph()
-	run := para.AddRun()
-	run.AddText(s)
-	return run
+	// 프레젠테이션 생성
+	ppt := presentation.New()
+
+	defer ppt.Close()
+
+	// 슬라이드 추가 및 데이터 설정
+	for _, slideData := range slides {
+		slide := ppt.AddSlide()
+
+		// 제목 설정
+		titleBox := slide.AddTextBox()
+		titlePara := titleBox.AddParagraph()
+		titleRun := titlePara.AddRun()
+		titleRun.SetText(slideData.Title)
+		titleBox.Properties().SetPosition(50, 50)
+		titleBox.Properties().SetSize(50, 50)
+
+		// 내용 설정
+		contentBox := slide.AddTextBox()
+		contentPara := contentBox.AddParagraph()
+		contentRun := contentPara.AddRun()
+		contentRun.SetText(slideData.Content)
+		contentBox.Properties().SetPosition(50, 50)
+		contentBox.Properties().SetSize(50, 50)
+	}
+
+	// 프레젠테이션 저장
+	filePath := "output.pptx"
+
+	err := ppt.SaveToFile(filePath)
+	if err != nil {
+		fmt.Println("Error saving presentation:", err)
+		return
+	}
+
+	sample := songInfo{}
+	sample.searchLyricsList("하나님은 너를 지키시는자")
+
+	fmt.Println("Presentation saved to", filePath)
 }
