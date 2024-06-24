6cb5b5a6bb379a8edb0d0d28e1b8861158911b11
ylhong <ylhong@ssrinc.co.kr>
2024-06-24 20:30:30 +0900

    ref

--- main.go
+++ main.go
@@ -17,53 +17,81 @@ type SlideData struct {
 	Content string
 }
 
-type songInfo struct {
-	trackId int
+type SongInfo struct {
+	TrackID int
+	Lyrics  string
 }
 
-func (si songInfo) searchLyricsList(recvStr string) {
-	// 검색어 설정
-	searchQuery := url.QueryEscape(recvStr)
-	searchUrl := fmt.Sprintf("https://music.bugs.co.kr/search/lyrics?q=%s", searchQuery)
-	fmt.Println(searchUrl)
+// searchLyricsList 함수는 가사 목록을 검색합니다.
+func (si *SongInfo) searchLyricsList(baseUrl, query string, isDirect bool) {
+	if si.Lyrics != "" {
+		return
+	}
+	searchUrl := formatSearchURL(baseUrl, query, isDirect)
+
 	// HTTP 요청 보내기
 	resp, err := http.Get(searchUrl)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalf("Failed to make HTTP request: %v", err)
 	}
 	defer resp.Body.Close()
 
 	if resp.StatusCode != 200 {
-		log.Fatalf("status code error: %d %s", resp.StatusCode, resp.Status)
+		log.Fatalf("Status code error: %d %s", resp.StatusCode, resp.Status)
 	}
 
 	// HTML 파싱
 	doc, err := goquery.NewDocumentFromReader(resp.Body)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalf("Failed to parse HTML: %v", err)
 	}
 
-	// 각 tr 요소를 탐색하며 albumid와 mark 태그의 텍스트를 추출
-	doc.Find("tr[rowtype='lyrics']").Each(func(i int, s *goquery.Selection) {
-		albumID, exists := s.Attr("trackId")
+	if isDirect {
+		si.parseLyrics(doc)
+	} else {
+		si.parseTrackList(doc)
+	}
+}
+
+// formatSearchURL 함수는 검색 URL을 생성합니다.
+func formatSearchURL(baseUrl, query string, isDirect bool) string {
+	if isDirect {
+		return fmt.Sprintf(baseUrl, query)
+	}
+	searchQuery := url.QueryEscape(query)
+	return fmt.Sprintf(baseUrl, searchQuery)
+}
+
+// parseTrackList 함수는 트랙 리스트를 파싱합니다.
+func (si *SongInfo) parseTrackList(doc *goquery.Document) {
+	doc.Find("table.trackList tbody tr[rowtype='lyrics']").Each(func(i int, s *goquery.Selection) {
+		albumID, exists := s.Attr("trackid")
 		if exists {
 			tempNo, err := strconv.ParseInt(albumID, 10, 64)
 			if err != nil {
-				log.Fatal(err)
+				log.Fatalf("Failed to parse track ID: %v", err)
 			}
-			si.trackId = int(tempNo)
-			fmt.Printf("AlbumID: %s ", albumID)
+			si.TrackID = int(tempNo)
+			si.searchLyricsList("https://music.bugs.co.kr/track/%s", albumID, true)
+
 		}
+
 	})
 }
+
+// parseLyrics 함수는 가사를 파싱합니다.
+func (si *SongInfo) parseLyrics(doc *goquery.Document) {
+	doc.Find(".lyricsContainer xmp").Each(func(i int, s *goquery.Selection) {
+		fmt.Println(s.Text())
+		si.Lyrics = s.Text()
+	})
+}
+
 func init() {
-	// Make sure to load your metered License API key prior to using the library.
-	// If you need a key, you can sign up and create a free one at https://cloud.unidoc.io
-	//468eb71b0f562ed29385b487b55d413ad506b3c48950ead1de75bd736c7c17c4
-	//err := license.SetMeteredKey(os.Getenv(`UNIDOC_LICENSE_API_KEY`))
+	// UniOffice 라이센스 설정
 	err := license.SetMeteredKey("468eb71b0f562ed29385b487b55d413ad506b3c48950ead1de75bd736c7c17c4")
 	if err != nil {
-		panic(err)
+		panic(fmt.Sprintf("Failed to set UniOffice license key: %v", err))
 	}
 }
 
@@ -75,12 +103,21 @@ func main() {
 		{"Title 3", "Content 3"},
 	}
 
-	// 프레젠테이션 생성
-	ppt := presentation.New()
+	// 프레젠테이션 생성 및 슬라이드 추가
+	createPresentation(slides, "output.pptx")
 
+	// 가사 검색
+	song := &SongInfo{}
+	song.searchLyricsList("https://music.bugs.co.kr/search/lyrics?q=%s", "하나님은 너를 지키시는자", false)
+
+	fmt.Println("Presentation saved to output.pptx")
+}
+
+// createPresentation 함수는 프레젠테이션을 생성하고 슬라이드를 추가합니다.
+func createPresentation(slides []SlideData, filePath string) {
+	ppt := presentation.New()
 	defer ppt.Close()
 
-	// 슬라이드 추가 및 데이터 설정
 	for _, slideData := range slides {
 		slide := ppt.AddSlide()
 
@@ -101,17 +138,7 @@ func main() {
 		contentBox.Properties().SetSize(50, 50)
 	}
 
-	// 프레젠테이션 저장
-	filePath := "output.pptx"
-
-	err := ppt.SaveToFile(filePath)
-	if err != nil {
-		fmt.Println("Error saving presentation:", err)
-		return
+	if err := ppt.SaveToFile(filePath); err != nil {
+		log.Fatalf("Error saving presentation: %v", err)
 	}
-
-	sample := songInfo{}
-	sample.searchLyricsList("하나님은 너를 지키시는자")
-
-	fmt.Println("Presentation saved to", filePath)
 }
