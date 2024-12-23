a5d368f08e1b861853b63a3b2499ca49d612a50b
AwesomeYelim <uiop01900@gmail.com>
2024-12-23 20:48:03 +0900

    temp_test

--- executor/bulletin/forPresentation/presentation.go
+++ executor/bulletin/forPresentation/presentation.go
@@ -9,8 +9,10 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/jung-kurt/gofpdf/v2"
+	"log"
 	"os"
 	"path/filepath"
+	"strconv"
 	"strings"
 )
 
@@ -39,22 +41,22 @@ func CreatePresentation(figmaInfo *get.Info, execPath string, config extract.Con
 	custom, err := os.ReadFile(filepath.Join(execPath, "config", "main_worship.json"))
 	err = json.Unmarshal(custom, &contents)
 
-	for _, con := range contents {
+	for i, con := range contents {
 		splitTitle := strings.Split(con.Title, "_")
 		objPdf.Title = splitTitle[1]
 
-		//var subT int
-		//var target int
-		//if strings.Contains(splitTitle[0], ".") {
-		//	subT, _ = strconv.Atoi(strings.Split(splitTitle[0], ".")[0])
-		//	if i+1 < len(contents) {
-		//		target, _ = strconv.Atoi(strings.Split(contents[i-1].Title, "_")[0])
-		//		if subT == target {
-		//			objPdf.CommonPath = objPdf.Title
-		//		}
-		//	}
-		//}
-		//log.Print("objPdf.CommonPath", objPdf.CommonPath)
+		var subT int
+		var target int
+		if strings.Contains(splitTitle[0], ".") {
+			subT, _ = strconv.Atoi(strings.Split(splitTitle[0], ".")[0])
+			if i+1 < len(contents) {
+				target, _ = strconv.Atoi(strings.Split(contents[i-1].Title, "_")[0])
+				if subT == target {
+					objPdf.CommonPath = objPdf.Title
+				}
+			}
+		}
+		log.Print("objPdf.CommonPath", objPdf.CommonPath)
 
 		if path, ok := figmaInfo.PathInfo[objPdf.Title]; ok {
 			objPdf.Path = filepath.Join(outputDir, filepath.Base(path))
